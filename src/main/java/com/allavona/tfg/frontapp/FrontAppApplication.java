c49a8419f093706bdaa30e349907d85a70122de1
Arturo Llavona Vallina <allavona@paradigmadigital.com>
2022-11-12 09:51:46 +0100

    Initial commit

---
+++ src/main/java/com/allavona/tfg/frontapp/FrontAppApplication.java
@@ -0,0 +1,13 @@
+package com.allavona.tfg.frontapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allavona/tfg/frontapp/FrontAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allavona.tfg.frontapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
